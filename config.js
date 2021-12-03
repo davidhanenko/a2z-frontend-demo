820b5a594d4f80ee6e68d50224d97a31c084ea2e
davidhanenko <d.hanenko24@gmail.com>
2021-12-02 19:31:00 -0500

    fixing 405, cors

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:1337/graphql`;
-export const prodEndpoint = `https://a2z-demo.herokuapp.com`;
+export const endpoint = 'http://localhost:1337/graphql';
+export const prodEndpoint = process.env.NEXT_PUBLIC_API_URL || 'https://a2z-demo.herokuapp.com';
 export const perPage = 4;
