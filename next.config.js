79cd88b146b8c1c0dccd83921fed564fddc09337
davidhanenko <d.hanenko24@gmail.com>
2021-12-10 23:42:08 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,43 +1,19 @@
 const withImages = require('next-images');
 
-// module.exports = withImages({
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/i,
-//         use: [
-//           {
-//             loader: 'file-loader',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   images: {
-//     domains: ['res.cloudinary.com'],
-//   },
-// });
-
-
-module.exports = {
-  async headers() {
-    return [
+module.exports = withImages({
+  module: {
+    rules: [
       {
-        source: 'https://a2z-demo.herokuapp.com',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/i,
+        use: [
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            loader: 'file-loader',
           },
         ],
       },
-    ];
+    ],
   },
-};
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+});
