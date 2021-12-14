352debe1d24bcf1591ea881ca88b28418db1cd03
davidhanenko <d.hanenko24@gmail.com>
2021-12-13 20:33:05 -0500

    test

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = 'http://localhost:1337/graphql';
 export const prodEndpoint =
-  process.env.NEXT_PUBLIC_API_URL || 'https://a2z-backend-demo.herokuapp.com';
+  process.env.NEXT_PUBLIC_API_URL ||
+  'https://a2z-backend-demo.herokuapp.com/graphql';
 export const perPage = 4;
