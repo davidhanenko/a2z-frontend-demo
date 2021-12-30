ec4a7ead1dbf44ff18d51432382bbae0c9312960
davidhanenko <d.hanenko24@gmail.com>
2021-12-30 00:57:06 -0500

    pagination

--- components/menu/MenuTree.js
+++ components/menu/MenuTree.js
@@ -12,9 +12,16 @@ export default function MenuTree() {
   // const { isActive, setIsActive } = useMenu();
 
   const router = useRouter();
+  // get route
   const routesArr = router.asPath.split('/').slice(1);
 
+  // remove url page query from last element of route 
+  routesArr[routesArr.length - 1] =
+    routesArr[routesArr.length - 1].split('?')[0];
+
+    // create link for each route node
   const getRoute = (r, i) => r.slice(0, i + 1).join('/');
+
 
   return (
     <MenuTreeStyles>
--- components/shared/pagination/Pagination.js
+++ components/shared/pagination/Pagination.js
@@ -11,10 +11,13 @@ export default function Pagination({ page, currentUrl, itemsCount }) {
       <Link href={`/${currentUrl}/?page=${+page - 1}`}>
         <a aria-disabled={page <= 1}>← Prev</a>
       </Link>
-      <p>
-        Page {page} of {pageCount}
-      </p>
-      <p> Items total</p>
+
+      {Array.from(Array(3)).map((el, i) => (
+        <Link key={i} href={`/${currentUrl}/?page=${i + 1}`}>
+          <a aria-disabled={page === i+1}>{i + 1}</a>
+        </Link>
+      ))}
+
       <Link href={`/${currentUrl}/?page=${+page + 1}`}>
         <a aria-disabled={page >= pageCount}>Next →</a>
       </Link>
