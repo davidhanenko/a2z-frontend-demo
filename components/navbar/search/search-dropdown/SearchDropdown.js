f7f71dc2f81037aebe666803decf195818d08af7
davidhanenko <d.hanenko24@gmail.com>
2022-02-05 00:50:47 -0500

    search dropdown

--- components/navbar/search/search-dropdown/SearchDropdown.js
+++ components/navbar/search/search-dropdown/SearchDropdown.js
@@ -20,55 +20,56 @@ export default function SearchDropdown({
   const handleClose = () => {
     return setTerm('');
   };
+  if (foundItems) {
+    return (
+      <SearchDropdownStyles>
+        {foundItems.map(item => (
+          <Link
+            key={item.id}
+            href={{
+              pathname: `/[service]/[items]/[collection]/[singleItem]`,
+              query: {
+                // service
+                service: `${formatUrlToRoute(
+                  item.category[0].items[0].services[0]
+                    .service
+                )}`,
+                // items category
+                items: `${formatUrlToRoute(
+                  item.category[0].items[0].title
+                )}`,
+                // items collection
+                collection: `${formatUrlToRoute(
+                  item.category[0].categoryTitle
+                )}`,
+                // single item/finded item
+                singleItem: `${formatUrlToRoute(
+                  item.itemTitle
+                )}`,
+              },
+            }}
+          >
+            <DropdownItemStyles>
+              <Image
+                src={item.image[0].url}
+                alt={item.itemTitle}
+                width={25}
+                height={25}
+              />
+              <p>{item.itemTitle}</p>
+            </DropdownItemStyles>
+          </Link>
+        ))}
 
-  return (
-    <SearchDropdownStyles>
-      {foundItems.map(item => (
-        <Link
-          key={item.id}
-          href={{
-            pathname: `/[service]/[items]/[collection]/[singleItem]`,
-            query: {
-              // service
-              service: `${formatUrlToRoute(
-                item.category[0].items[0].services[0]
-                  .service
-              )}`,
-              // items category
-              items: `${formatUrlToRoute(
-                item.category[0].items[0].title
-              )}`,
-              // items collection
-              collection: `${formatUrlToRoute(
-                item.category[0].categoryTitle
-              )}`,
-              // single item/finded item
-              singleItem: `${formatUrlToRoute(
-                item.itemTitle
-              )}`,
-            },
-          }}
-        >
-          <DropdownItemStyles>
-            <Image
-              src={item.image[0].url}
-              alt={item.itemTitle}
-              width={25}
-              height={25}
-            />
-            <p>{item.itemTitle}</p>
-          </DropdownItemStyles>
-        </Link>
-      ))}
-
-      <AllResultsStyles>
-        {foundItemsCount !== 0
-          ? foundItemsCount
-          : `Nothing found`}
-      </AllResultsStyles>
-      <CloseBtnStyles type='button' onClick={handleClose}>
-        <MdOutlineClose />
-      </CloseBtnStyles>
-    </SearchDropdownStyles>
-  );
+        <AllResultsStyles>
+          {foundItemsCount !== 0
+            ? foundItemsCount
+            : `Nothing found`}
+        </AllResultsStyles>
+        <CloseBtnStyles type='button' onClick={handleClose}>
+          <MdOutlineClose />
+        </CloseBtnStyles>
+      </SearchDropdownStyles>
+    );
+  }
 }
